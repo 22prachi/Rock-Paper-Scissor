de3fb0119491d829f3b54e05d71b381f9cde151f
Prachi Mukati <160760200+22prachi@users.noreply.github.com>
2024-07-08 21:50:25 +0530

    Add files via upload

---
+++ rock.js
@@ -0,0 +1,125 @@
+
+const youPoints=document.querySelector('.you-points')
+const paper=document.querySelector(".paper");
+const rock=document.querySelector(".rock");
+const scissor=document.querySelector(".scissor");
+const result=document.querySelector('.resultPara')
+const compPoints=document.querySelector('.comp-points')
+paper.addEventListener('click',function(){
+    check1()
+})
+rock.addEventListener('click',function(){
+    check2()
+})
+scissor.addEventListener('click',function(){
+    check3()
+})
+
+let score=0
+let compScore=0
+
+function check1(){
+   
+  const computer=compMove()
+  console.log(computer)
+  if (computer === 'paper'){
+     youPoints.innerText=`0`
+     compPoints.innerText=`0`
+     
+     result.innerHTML='It was Draw'
+     result.style.backgroundColor='orange'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+
+  }else if(computer === 'scissor'){
+    compScore++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You lost. Scissor cuts Paper`
+     result.style.backgroundColor='red'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }else{
+    score++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You won. Paper holds rock`
+     result.style.backgroundColor='green'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }
+}
+function check2(){
+    let score=0
+  const computer=compMove()
+  console.log(computer)
+  if (computer === 'rock'){
+     youPoints.innerText=`0`
+     compPoints.innerText=`0`
+   
+     result.innerHTML='It was Draw'
+     result.style.backgroundColor='orange'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+
+  }else if(computer === 'paper'){
+    compScore++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You lost . Paper holds rock`
+     result.style.backgroundColor='red'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }else{
+    score++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You won. Rock beats scissor`
+     result.style.backgroundColor='green'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }
+}
+
+function check3(){
+    let score=0
+  const computer=compMove()
+  console.log(computer)
+  if (computer === 'scissor'){
+     youPoints.innerText=`0`
+     compPoints.innerText=`0`
+
+     result.innerHTML='It was Draw'
+     result.style.backgroundColor='orange'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+
+  }else if(computer === 'rock'){
+    compScore++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You lost. Rock beats scissor`
+     result.style.backgroundColor='red'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }else{
+    score++
+    youPoints.innerHTML=`${score}`
+    compPoints.innerHTML=`${compScore}`
+     result.innerHTML=`You won. Scissor cuts paper`
+     result.style.backgroundColor='green'
+     result.style.width='400px'
+     result.style.fontSize='18px'
+  }
+}
+function compMove(){
+    const randomNo=Math.floor(Math.random()*3 +1)
+    if(randomNo===1){
+        return 'paper'
+    }
+    else if(randomNo===2){
+        return 'rock'
+    }else{
+        return 'scissor'
+    }
+}
